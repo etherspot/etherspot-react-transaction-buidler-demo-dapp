bc8695a17018d53e95bdb441328be140d2934a78
poocart <as.deimis@gmail.com>
2022-09-13 12:26:18 +0300

    dashboard theme

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,13 @@ import styled from 'styled-components';
 
 const wallets = [
   { walletName: "metamask", preferred: true },
+  {
+    walletName: "walletConnect",
+    preferred: true,
+    rpc: {
+      1: `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`,
+    }
+  },
 ];
 
 const chainId = 1;
@@ -38,9 +45,25 @@ const ConnectWalletButton = styled.span`
     opacity: 0.4;
   }
 `;
+const ToggleThemeButton = styled.span`
+  padding: 10px;
+  display: inline-block;
+  border: 1px solid #000;
+  text-transform: uppercase;
+  font-size: 12px;
+  margin-right: 20px;
+  font-family: "Arial", sans;
+  cursor: pointer;
+  margin-bottom: 15px;
+
+  &:hover {
+    opacity: 0.4;
+  }
+`;
 
 const App = () => {
   const [connectedProvider, setConnectedProvider] = useState(null);
+  const [useDashboardTheme, setUseDashboardTheme] = useState(false);
 
   const walletService = useMemo(() => WalletService(), []);
 
@@ -56,6 +79,53 @@ const App = () => {
     setConnectedProvider(wallet);
   }, []);
 
+  const themeOverride = useMemo(() => {
+    if (!useDashboardTheme) return undefined;
+    return {
+        color: {
+          background: {
+            main: '#221f33',
+            topMenu: '#443d66',
+            topMenuButton: '#ff884d',
+            card: '#2b2640',
+            button: '#ff884d',
+            closeButton: '#ff884d',
+            selectInputToggleButton: '#ff884d',
+            selectInput: '#443d66',
+            selectInputExpanded: '#1a1726',
+            selectInputImagePlaceholder: '#443d66',
+            textInput: '#1a1726',
+            switchInput: '#1a1726',
+            switchInputActiveTab: '#443d66',
+            switchInputInactiveTab: 'transparent',
+          },
+          text: {
+            selectInput: '#ffeee6',
+            selectInputOption: '#ffeee6',
+            selectInputOptionSecondary: '#ffeee6',
+            searchInput: '#998ae6',
+            searchInputSecondary: '#998ae6',
+            outerLabel: '#998ae6',
+            innerLabel: '#998ae6',
+            topMenu: '#998ae6',
+            main: '#998ae6',
+            topBar: '#998ae6',
+            buttonSecondary: '#998ae6',
+            card: '#ffeee6',
+            cardTitle: '#ffeee6',
+            button: '#fff',
+            errorMessage: '#ff4d6a',
+            textInput: '#ffeee6',
+            textInputSecondary: '#ffeee6',
+            switchInputActiveTab: '#ffeee6',
+            switchInputInactiveTab: '#bbb8cc',
+            selectInputImagePlaceholder: '#ffeee6',
+            cardDisabled: '#605e5e',
+          }
+        }
+      }
+  }, [useDashboardTheme]);
+
   return (
     <>
       {!connectedProvider && (
@@ -67,10 +137,12 @@ const App = () => {
       )}
       {connectedProvider && (
         <div>
+          <ToggleThemeButton onClick={() => setUseDashboardTheme(!useDashboardTheme)}>Toggle theme</ToggleThemeButton>
           <Etherspot
             defaultTransactionBlocks={[{ type: TRANSACTION_BLOCK_TYPE.ASSET_BRIDGE }]}
             provider={connectedProvider}
             chainId={chainId}
+            themeOverride={themeOverride}
           />
         </div>
       )}
