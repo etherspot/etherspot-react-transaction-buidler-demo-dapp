c3fdf5ef4f61dafc63f02014ae5628148b61be1d
Ardian <34317628+arddluma@users.noreply.github.com>
2022-11-23 15:24:52 +0100

    Add npm install command in build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 if [ "$CF_PAGES_BRANCH" == "develop" ]; then
   jq '.dependencies."@etherspot/react-transaction-buidler" =  "etherspot/etherspot-react-transaction-buidler#develop"' package.json > temp-package.json && mv temp-package.json package.json
+  npm install
   npm run build
 else
   npm run build
--- preinstall.sh
+++
@@ -1,3 +0,0 @@
-# !/bin/bash
-echo "Running pre-install script"
-jq '.dependencies."@etherspot/react-transaction-buidler" =  "etherspot/etherspot-react-transaction-buidler#develop"' package.json > temp-package.json && mv temp-package.json package.json
