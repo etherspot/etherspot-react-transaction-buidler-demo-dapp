88983d7c646ea746b35f77667e625832db09ab07
Ardian <34317628+arddluma@users.noreply.github.com>
2022-11-23 16:42:18 +0100

    Switch back to npm

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 if [ "$CF_PAGES_BRANCH" == "develop" ]; then
   echo "Branch is:" $CF_PAGES_BRANCH
   jq '.dependencies."@etherspot/react-transaction-buidler" =  "github:https://'"$ETHERSPOT_BOT_GITHUB_TOKEN}"'@github.com/etherspot/etherspot-react-transaction-buidler.git#develop"' package.json > temp-package.json && mv temp-package.json package.json
-  yarn install
-  yarn run build
+  npm install
+  npm run build
 else
-  yarn run build
+  npm run build
 fi
