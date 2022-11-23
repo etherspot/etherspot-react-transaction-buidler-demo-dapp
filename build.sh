6245ef31ab7db3eb5d88d504a4ac1eb4a576a8d5
Ardian <34317628+arddluma@users.noreply.github.com>
2022-11-23 16:36:28 +0100

    Temporary switch to yarn

--- build.sh
+++ build.sh
@@ -2,10 +2,9 @@
 
 if [ "$CF_PAGES_BRANCH" == "develop" ]; then
   echo "Branch is:" $CF_PAGES_BRANCH
-  git config --global url."https://".insteadOf ssh://
   jq '.dependencies."@etherspot/react-transaction-buidler" =  "git+https://'"$ETHERSPOT_BOT_GITHUB_TOKEN}"'@github.com/etherspot/etherspot-react-transaction-buidler.git#develop"' package.json > temp-package.json && mv temp-package.json package.json
-  npm install
-  npm run build
+  yarn install
+  yarn run build
 else
-  npm run build
+  yarn run build
 fi
