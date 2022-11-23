3477cfc3f6b37d354f72a4c01ad83b5114a6846b
MayukhSamadder2000 <mayukhmmm@gmail.com>
2022-11-23 22:46:04 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -102,6 +102,8 @@ const App = () => {
             switchInputInactiveTab: 'transparent',
             pill: '#2b2640',
             checkboxInputInactive: '#665c99',
+            toDropdownColor: "#292539",
+            selectInputExpandedHover: "#292539",
           },
           text: {
             selectInput: '#ffeee6',
