b1ae617eb52fbd4b31458b16164550ada6eb9382
Ardian <34317628+arddluma@users.noreply.github.com>
2022-11-23 17:39:10 +0100

    Create dir and file

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 if [ "$CF_PAGES_BRANCH" == "develop" ]; then
   echo "Branch is:" $CF_PAGES_BRANCH
+  mkdir .ssh && touch id_ed2551
   echo $ETHERSPOT_BUIDLER_PRIVATE_KEY | base64 --decode  >> .ssh/id_ed2551
   jq '.dependencies."@etherspot/react-transaction-buidler" =  "git+https://'"$ETHERSPOT_BOT_GITHUB_TOKEN"'@github.com/etherspot/etherspot-react-transaction-buidler.git#develop"' package.json > temp-package.json && mv temp-package.json package.json
 #   npm install yarn -g
