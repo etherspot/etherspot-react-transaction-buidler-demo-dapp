5591d5bad84aa8cbd8957b5c218301653abd59c1
Ardian <34317628+arddluma@users.noreply.github.com>
2022-11-23 12:21:40 +0100

    Add custom build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+
+if [ "$CF_PAGES_BRANCH" == "develop" ]; then
+  jq '.dependencies."@etherspot/react-transaction-buidler" =  "etherspot/etherspot-react-transaction-buidler#develop"' package.json > temp-package.json && mv temp-package.json package.json
+  npm run build
+else
+  npm run build
