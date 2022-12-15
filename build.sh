c44184aac8e53d9b791834eb5cad8867d5157b19
Ardian <34317628+arddluma@users.noreply.github.com>
2022-12-15 16:41:53 +0100

    Add Unix timestamp variable

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ if [ "$CF_PAGES_BRANCH" == "develop" ]; then
   mkdir .ssh && touch id_ed2551
   echo $ETHERSPOT_BUIDLER_PRIVATE_KEY | base64 --decode  >> .ssh/id_ed2551
   jq '.dependencies."@etherspot/react-transaction-buidler" =  "git+https://'"$ETHERSPOT_BOT_GITHUB_TOKEN"'@github.com/etherspot/etherspot-react-transaction-buidler.git#develop"' package.json > temp-package.json && mv temp-package.json package.json
-#   npm install yarn -g
+  BUILD_TIMESTAMP=$(date +%s)
+  echo "BUILD_TIMESTAMP=$BUILD_TIMESTAMP" >> .env
   npm install
   NODE_ENV=production npm run build
 else
