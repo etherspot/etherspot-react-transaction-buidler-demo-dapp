bb93e7246707e915b67ac4e638a5cf87363e079c
Ardian <34317628+arddluma@users.noreply.github.com>
2022-12-15 19:01:30 +0100

    Add REACT_APP prefix

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if [ "$CF_PAGES_BRANCH" == "develop" ]; then
   echo $ETHERSPOT_BUIDLER_PRIVATE_KEY | base64 --decode  >> .ssh/id_ed2551
   jq '.dependencies."@etherspot/react-transaction-buidler" =  "git+https://'"$ETHERSPOT_BOT_GITHUB_TOKEN"'@github.com/etherspot/etherspot-react-transaction-buidler.git#develop"' package.json > temp-package.json && mv temp-package.json package.json
   BUILD_TIMESTAMP=$(date +%s)
-  echo "BUILD_TIMESTAMP=$BUILD_TIMESTAMP" >> .env
+  echo "REACT_APP_BUILD_TIMESTAMP=$BUILD_TIMESTAMP" >> .env
   npm install
   NODE_ENV=production npm run build
 else
