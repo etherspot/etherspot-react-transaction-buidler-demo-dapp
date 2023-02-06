54cf2f3c3b1439c7bbe6f006dbff5b8274957487
Yash-Chaubal <yashschaubal@gmail.com>
2023-02-06 16:19:19 +0000

    changed theme override

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import React, {
-  useMemo,
-  useState,
-} from 'react';
+import React, { useMemo, useState } from 'react';
 import { Etherspot, TRANSACTION_BLOCK_TYPE } from '@etherspot/react-transaction-buidler';
 import styled, { createGlobalStyle } from 'styled-components';
 import Web3 from 'web3';
@@ -35,7 +32,7 @@ const ToggleThemeButton = styled.span`
   text-transform: uppercase;
   font-size: 12px;
   margin-right: 20px;
-  font-family: "Arial", sans;
+  font-family: 'Arial', sans;
   cursor: pointer;
   margin-bottom: 15px;
   color: #fff;
@@ -53,54 +50,77 @@ const App = () => {
   const themeOverride = useMemo(() => {
     if (!useDashboardTheme) return undefined;
     return {
-        color: {
-          background: {
-            main: '#221f33',
-            topMenu: '#443d66',
-            topMenuButton: '#ff884d',
-            card: '#2b2640',
-            button: '#ff884d',
-            closeButton: '#ff884d',
-            selectInputToggleButton: '#ff884d',
-            selectInput: '#443d66',
-            selectInputExpanded: '#1a1726',
-            selectInputImagePlaceholder: '#443d66',
-            textInput: '#1a1726',
-            switchInput: '#1a1726',
-            switchInputActiveTab: '#443d66',
-            switchInputInactiveTab: 'transparent',
-            pill: '#2b2640',
-            checkboxInputInactive: '#665c99',
-            dropdownHoverColor: "#443d66",
-            selectInputExpandedHover: "#443d66",
-          },
-          text: {
-            selectInput: '#ffeee6',
-            selectInputOption: '#ffeee6',
-            selectInputOptionSecondary: '#ffeee6',
-            searchInput: '#998ae6',
-            searchInputSecondary: '#998ae6',
-            outerLabel: '#998ae6',
-            innerLabel: '#998ae6',
-            topMenu: '#998ae6',
-            main: '#ffeee6',
-            topBar: '#998ae6',
-            buttonSecondary: '#998ae6',
-            card: '#ffeee6',
-            cardTitle: '#ffeee6',
-            button: '#fff',
-            errorMessage: '#ff4d6a',
-            textInput: '#ffeee6',
-            textInputSecondary: '#ffeee6',
-            switchInputActiveTab: '#ffeee6',
-            switchInputInactiveTab: '#bbb8cc',
-            selectInputImagePlaceholder: '#ffeee6',
-            cardDisabled: '#605e5e',
-            pill: '#bbb8cc',
-            pillValue: '#ffeee6',
-          }
-        }
-      }
+      color: {
+        background: {
+          main: 'linear-gradient(to right, #f43b40, #f8793f)',
+          card: '#fff7f2',
+          topMenu: '#fff',
+          topMenuButton: '#fff',
+          selectInput: '#fff',
+          selectInputExpanded: '#fff',
+          selectInputScrollbar: '#ff7733',
+          selectInputScrollbarHover: 'rgba(255, 119, 51, 0.8)',
+          selectInputScrollbarActive: 'rgba(255, 119, 51, 0.5)',
+          selectInputImagePlaceholder: '#ffe6d9',
+          selectInputToggleButton: '#0a1427',
+          textInput: '#ffe6d9',
+          switchInput: '#ffd2bb',
+          switchInputActiveTab: '#fff',
+          switchInputInactiveTab: 'transparent',
+          button: '#fff',
+          closeButton: '#0a1427',
+          pill: '#fff7f2',
+          roundedImageFallback: '#ffe6d9',
+          listItemQuickButtonSecondary: '#443d66',
+          listItemQuickButtonPrimary: '#ff884d',
+          statusIconSuccess: '#1ba23d',
+          statusIconPending: '#ff6b35',
+          statusIconFailed: '#ff0000',
+          checkboxInputActive: '#ff884d',
+          checkboxInputInactive: '#7f7a99',
+          dropdownHoverColor: '#F8EFEA',
+          selectInputExpandedHover: '#F8EFEA',
+          toDropdownColor: '#F8EFEA',
+          secondary: '#9889e4',
+          selectInputRadioOn: '#ff7733',
+          selectInputRadioOff: '#F8EFEA',
+          walletButton: 'linear-gradient(to bottom, #fd9250, #ff5548)',
+          walletChainDropdown: `#fff`,
+          walletChainButtonActive: '#ffeee6',
+        },
+        text: {
+          main: '#fff',
+          topBar: '#fff',
+          topMenu: '#191726',
+          cardTitle: '#191726',
+          card: '#000',
+          cardDisabled: '#ddd',
+          innerLabel: '#6e6b6a',
+          outerLabel: '#6e6b6a',
+          selectInput: '#000',
+          selectInputOption: '#191726',
+          selectInputOptionSecondary: '#191726',
+          selectInputImagePlaceholder: '#6e6b6a',
+          textInput: '#000',
+          textInputSecondary: '#6e6b6a',
+          switchInputActiveTab: '#191726',
+          switchInputInactiveTab: '#6e6b6a',
+          button: '#191726',
+          buttonSecondary: '#ffeee6',
+          errorMessage: '#ff0000',
+          searchInput: '#ff7733',
+          searchInputSecondary: '#ff7733',
+          pill: '#6e6b6a',
+          pillValue: '#191726',
+          roundedImageFallback: '#6e6b6a',
+          listItemQuickButtonSecondary: '#fff',
+          listItemQuickButtonPrimary: '#fff',
+          transactionStatusLink: '#ff7733',
+          pasteIcon: '#ff884d',
+          walletDropdownIcon: '#221f33',
+        },
+      },
+    };
   }, [useDashboardTheme]);
 
   return (
@@ -117,7 +137,7 @@ const App = () => {
 
               const web3 = new Web3(web3Provider as any);
               // @ts-ignore
-              setConnectedProvider(web3.currentProvider)
+              setConnectedProvider(web3.currentProvider);
             }}
             onWeb3AuthInstanceSet={setWeb3AuthInstance}
           />
@@ -149,6 +169,6 @@ const App = () => {
       </Wrapper>
     </>
   );
-}
+};
 
 export default App;
