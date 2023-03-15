44081c345dbd582ba83a09837d4a59e30be90bd3
poocart <7067483+poocart@users.noreply.github.com>
2023-03-15 20:24:29 +0200

    checkout from fixes branch

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ if [ "$CF_PAGES_BRANCH" == "v2-staking-demo" ]; then
   echo "Branch is:" $CF_PAGES_BRANCH
   mkdir .ssh && touch id_ed2551
   echo $ETHERSPOT_BUIDLER_PRIVATE_KEY | base64 --decode  >> .ssh/id_ed2551
-  jq '.dependencies."@etherspot/react-transaction-buidler" =  "git+https://'"$ETHERSPOT_BOT_GITHUB_TOKEN"'@github.com/etherspot/etherspot-react-transaction-buidler.git#develop"' package.json > temp-package.json && mv temp-package.json package.json
+  jq '.dependencies."@etherspot/react-transaction-buidler" =  "git+https://'"$ETHERSPOT_BOT_GITHUB_TOKEN"'@github.com/etherspot/etherspot-react-transaction-buidler.git#fix/pillar-staking-v2"' package.json > temp-package.json && mv temp-package.json package.json
   BUILD_TIMESTAMP=$(date +%s)
   echo "REACT_APP_BUILD_TIMESTAMP=$BUILD_TIMESTAMP" >> .env
   npm install
